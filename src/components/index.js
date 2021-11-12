cbbcee4d361720794e3bef1d28b675cffdff2fad
mvn-huyphan-dn <huy.phan@monstar-lab.com>
2021-11-12 17:49:39 +0700

    Dashboard Page

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import ProvideAuth from './ProvideAuth'
 import CustomHeader from './Header'
 import SideBar from './Sidebar'
 import DatePicker from './DatePicker'
+import Calendar from './Calendar'
 
 export {
   ProvideAuth,
   CustomHeader,
   SideBar,
-  DatePicker
+  DatePicker,
+  Calendar
 }
